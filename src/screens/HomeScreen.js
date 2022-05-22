3cbe2347b57ca966a95d0a51bdc59a27def10a3e
Eduardo Alves <31996686+alvesil@users.noreply.github.com>
2022-05-22 02:13:18 -0300

    primeiro upload

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+import Text from "../components/Text";
+
+export default HomeScreen = () => {
+  return (
+    <View>
+        <Text>Home Screen</Text>
+    </View>
+  );
+}
